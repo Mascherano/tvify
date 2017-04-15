7b5f7f9e97f7299074029871e3d3920cd9051781
marcelo <chelomario@gmail.com>
2017-04-15 19:29:00 -0400

    cambios para pruebas de produccion

--- src/render/index.js
+++ src/render/index.js
@@ -1,28 +1,31 @@
+/**
+ * Module Dependencies
+ */
+
 import $ from 'jquery'
-import $tvShowsContainer from '../tv-shows-container'
+import $tvShowsContainer from 'src/tv-shows-container'
 
-var template = '<article class="tv-show">' +
-					'<div class="left img-container">' +
-						'<img src=":img:" alt=":img alt:">' +
-					'</div>' +
-					'<div class="right info">' +
-						'<h2>:name:</h2>' +
-						'<p>:summary:</p>' +
-						'<button class="like">like</button>'+
-					'</div>' +
-				'</article>';
+var template = `<article class="tv-show">
+          <div class="left img-container">
+            <img src=":img:" alt=":img alt:">
+          </div>
+          <div class="right info">
+            <h1>:name:</h1>
+            <p>:summary:</p>
+            <button class="like">💖</button
+          </div>
+        </article>`
 
-export default function renderShows(shows){
-	$tvShowsContainer.find('.loader').remove();
-	shows.forEach(function (show){
-		var article = template
-			.replace(':name:', show.name)
-			.replace(':img:', show.image ? show.image.medium : '')
-			.replace(':summary:', show.summary)
-			.replace(':img alt:', show.name + ' Logo')
+export default function renderShows(shows = []) {
+  $tvShowsContainer.find('.loader').remove();
+  shows.forEach(function (show) {
+    var article = template
+      .replace(':name:', show.name)
+      .replace(':img:', show.image ? show.image.medium : '')
+      .replace(':summary:', show.summary)
+      .replace(':img alt:', show.name + " Logo")
 
-		var $article = $(article)
-		$article.hide();
-		$tvShowsContainer.append($article.fadeIn(2000));
-	})
+    var $article = $(article)
+    $tvShowsContainer.append($article.fadeIn(1500));
+  })
 }
